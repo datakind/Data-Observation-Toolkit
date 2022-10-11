2e3679834dda39d61b5e63db5168a1efd1a1508d
Matthew Harris <matthew@regolit.org>
2022-10-11 18:50:32 -0400

    Migration from DataKind DataCorps private repo

--- docker/run_dot_demo.sh
+++ docker/run_dot_demo.sh
@@ -2,6 +2,10 @@
 # Script to download data file and run DOT demo.
 #
 #!/bin/bash
+
+# First clean up any previous demos
+cd demo && rm -rf ./db && rm -rf ./appsmith && rm *.tar* && cd ..
+
 echo "Downloading demo data file ..."
 # See scrip upload_demo_to_dockerhub.sh to see how this URL file was created
 # File is in this folder: https://drive.google.com/drive/folders/18p5Alxy7wR7cO6w4FD_o3zIsXHCe-RyR
@@ -20,6 +24,12 @@ docker compose -f docker-compose-demo.yml stop
 docker compose -f docker-compose-demo.yml build
 docker compose -f docker-compose-demo.yml up -d
 
+echo "Waiting for DOT to start, time to make a nice cup of tea! ☕ 😃 ..."
+sleep 30
+
 # This is the APP shared with 'public' set to on, so use doesn't have to log in
-echo "Open a browser and go to this URL: http://localhost:82/app/data-observation-toolkit/run-log-634491ea0da61b0e9f38760d"
+echo "Open a browser and go to this URL: "
+echo
+echo "         http://localhost:82/app/data-observation-toolkit/run-log-634491ea0da61b0e9f38760d?embed=True"
+echo
 echo "To STOP DOT run this command: docker compose -f docker-compose-demo.yml stop"
