37b4fb45fd96cec495d09466f136661893234cf2
janpeter <jan.peter90@gmail.com>
2022-11-14 17:19:23 -0500

    Use python-on-whale package to create and run containers

--- docker/run_demo.py
+++ docker/run_demo.py
@@ -1,23 +1,23 @@
 '''This script will run the demo regardless of the users OS.Therefore,only this one file has to be maintained
-instead of one ps1 for windows and one sh for linux
+
 Steps the script will have to do:
 1)Download zipped tarfile from google drive (Hint:this should be done directly through python)
-2)Unzip and untar it(HINT:Should also be possible through python-if not,import)
-3)Composedocker Containers
+2)Unzip and untar it
+3)Compose and run docker Containers
 4)Access DOT-demo under http://localhost:82/app/data-observation-toolkit/run-log-634491ea0da61b0e9f38760d?embed=True
-TIPP:Avoid system commands in python code but use libraries etc.!
 '''
 
 import os
 import shutil
 import gdown
 import tarfile
-import docker
+import time  # imported only to wait until server is up...
+from python_on_whales import DockerClient  # had to be pip installed
 
-#Had to be pip installed
-#Check if db, appsmith and tar file are there and if so, delete them.
-#Idea: Use path relative to this file - has to be changed if the file is moved
+# Set environment variable for later use
+os.environ['POSTGRES_PASSWORD'] = 'password125'
 
+# Check if db, appsmith and tar file are there and if so, delete them.
 os.chdir("demo/")
 if os.path.exists("db"):
     shutil.rmtree("db")
@@ -26,27 +26,24 @@ if os.path.exists("appsmith"):
 if os.path.exists("dot_demo_data.tar"):
     os.remove("dot_demo_data.tar")
 
-#Download Demo Data from Google Drive
-#QUESTION: Can import gdown library alternatively - any pros or cons?
+# Download Demo Data from Google Drive
 print("Downloading demo data file ...")
 url = "https://drive.google.com/uc?id=157Iad8mHnwbZ_dAeLQy5XfLihhcpD6yc"
 output = "dot_demo_data.tar.gz"
 gdown.download(url, output, quiet=False)
-print("download successful")
+print("Download successful")
 
-#Open/Extract tarfile
+# Open/Extract tarfile
 my_tar = tarfile.open('dot_demo_data.tar.gz')
-my_tar.extractall('') # specify which folder to extract to
+my_tar.extractall('')  # specify which folder to extract to
 my_tar.close()
 
-#Starting DOT (composing Container)
-#os.system("cd.. && docker compose -f docker-compose-demo.yml build && docker compose -f docker-compose-demo.yml up -d")
-
-print(os.curdir)
-client = docker.from_env()
-client.containers.create(image='alpine')
-
+# Composing and running container(s)
 print("Starting DOT ...")
-print("Waiting for DOT to start, time to make a nice cup of tea! ☕ 😃 ...")
-print("Open a browser and go to this URL: \n http://localhost:82/app/data-observation-toolkit/run-log-634491ea0da61b0e9f38760d?embed=True")
-print("To STOP DOT run this command: docker compose -f docker-compose-demo.yml stop")
+os.chdir("../")
+docker = DockerClient(compose_files=[os.getcwd() + os.sep + "docker-compose-demo.yml"])
+docker.compose.down()
+docker.compose.build(quiet=True)
+time.sleep(20)
+docker.compose.up(quiet=True)
+
