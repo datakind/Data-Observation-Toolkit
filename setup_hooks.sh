f22b607f5e6d2e786d1a648b75db74364bfe78f4
Lorenzo Rubio <lrnzcig@gmail.com>
2022-11-18 22:00:12 +0100

    typos -add integration tests to GH actions

--- setup_hooks.sh
+++ setup_hooks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $'#/bin/sh\nblack .\nfor f in `git diff --name-only | grep ".py"`; do python lint.py -p $f; done; pytest dot/self_tests/unit; pytest dot/self_tests/integration';> .git/hooks/pre-commit
+echo $'#/bin/sh\nblack .\nfor f in `git diff --name-only | grep ".py"`; do python lint.py -p $f; done; pytest dot/self_tests/unit; pytest dot/self_tests/integration'> .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
