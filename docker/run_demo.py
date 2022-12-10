72c55d8e3d0fd6c6228a2329dcb0d80091eb667b
Lorenzo Rubio <lrnzcig@gmail.com>
2022-12-10 21:04:01 +0100

    improve pylint scores

--- docker/run_demo.py
+++ docker/run_demo.py
@@ -10,7 +10,7 @@ from python_on_whales import DockerClient
 
 url_demo_data = "https://drive.google.com/uc?id=157Iad8mHnwbZ_dAeLQy5XfLihhcpD6yc"
 filename_demo_data = "dot_demo_data.tar.gz"
-url_dot_ui = "http://localhost:82/app/data-observation-toolkit/run-log-634491ea0da61b0e9f38760d?embed=True"
+url_dot_ui = "http://localhost:82/app/data-observation-toolkit/run-log-634491ea0da61b0e9f38760d?embed=True"  # pylint: disable=line-too-long
 
 # Check if db, appsmith and tar file are there and if so, delete them.
 os.chdir("demo/")
@@ -30,12 +30,12 @@ print("Demo data has been downloaded\n")
 
 # Open/Extract tarfile
 with tarfile.open(filename_demo_data) as my_tar:
-    my_tar.extractall('')
+    my_tar.extractall("")
     my_tar.close()
 
 with open("./db/.env") as f:
-    demo_pwd=f.read().split("=")[1]
-    os.environ['POSTGRES_PASSWORD'] = demo_pwd
+    demo_pwd = f.read().split("=")[1]
+    os.environ["POSTGRES_PASSWORD"] = demo_pwd
 
 # Composing and running container(s)
 print("Starting DOT...\n")
@@ -49,8 +49,10 @@ time.sleep(20)
 
 webbrowser.open(url_dot_ui)
 
-print("In case DOT was not opened in your browser, please go to this URL: "
-      "http://localhost:82/app/data-observation-toolkit/run-log-634491ea0da61b0e9f38760d?embed=True\n")
+print(
+    "In case DOT was not opened in your browser, please go to this URL: "
+    "http://localhost:82/app/data-observation-toolkit/run-log-634491ea0da61b0e9f38760d?embed=True\n"
+)
 input("Press return to stop DOT container\n")
 print("Container is being stopped - we hope you enjoyed this demo :)")
 docker.compose.stop()
