13b7299691c3029aaa7d000d9d5cb4cb31bde186
janer <Jan.Peter@Datakind.org>
2023-04-06 16:54:33 -0400

    DKW-1263: Included Appsmith in docker compose file for airflow. Added SQL INSERTS to create - and populate - tables for DB connection(s). Also updated ReadMe to mention the use of .env files for passwords.

---
+++ db/dot/5-db_connections.sql
@@ -0,0 +1,25 @@
+CREATE SCHEMA IF NOT EXISTS dot_config;
+
+CREATE TABLE dot_config.dot(
+output_schema_suffix VARCHAR(255) NOT NULL DEFAULT 'test'
+);
+
+INSERT INTO dot_config.dot(output_schema_suffix)
+VALUES('tests');
+
+CREATE TABLE dot_config.dot_db_config(
+connection_name VARCHAR(255) PRIMARY KEY,
+type VARCHAR(255),
+host VARCHAR(255),
+user_name VARCHAR(255),
+password VARCHAR(255),
+port VARCHAR(255),
+dbname VARCHAR(255),
+schema VARCHAR(255),
+threads VARCHAR(255)
+);
+
+INSERT INTO dot_config.dot_db_config(connection_name,type,host,user_name,password,port,dbname,schema,threads)
+VALUES('dot_db','postgres','dot_db','postgres','','5432','dot_db','dot','4');
+
+--ToDo:Hashthepasswordcolumn
